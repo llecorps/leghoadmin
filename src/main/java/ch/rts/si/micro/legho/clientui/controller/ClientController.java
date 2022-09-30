3a3ba2a18e042568902ad937ae22b2d1eff047f2
esspressoh <laurent.lecorps@gmail.com>
2022-09-30 12:14:20 +0200

    add login form

--- src/main/java/ch/rts/si/micro/legho/clientui/controller/ClientController.java
+++ src/main/java/ch/rts/si/micro/legho/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import ch.rts.si.micro.legho.clientui.model.Listuser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -28,5 +29,9 @@ public class ClientController {
         List<Listuser> listusers = listuserDao.findAll();
         model.addAttribute( "listusers", listusers );
         return "listuser";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
